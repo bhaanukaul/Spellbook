637ca613f3f8c11ca77c4990537bfb853c83cfe1
Bhaanu Kaul <bhaanu@bhaanukaul.com>
2021-11-25 16:51:22 -0600

    Initialize the Spellbook. Creates ini file with db location. Creates empty db and intializes a spells table.

--- internal/Spell/Spell.go
+++ internal/Spell/Spell.go
@@ -3,25 +3,18 @@ package Spell
 import (
 	"Spellbook/internal/Utils"
 	"fmt"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
-type SpellDBModel struct {
+var tableName = "spells"
+
+type Spell struct {
 	ID          int    `json:"id,omitempty"`
 	Language    string `json:"language"`
 	Contents    string `json:"contents"`
 	Description string `json:"description"`
 	Tags        string `json:"tags"`
-}
-
-type Spell struct {
-	ID          int      `json:"id,omitempty"`
-	Language    string   `json:"language"`
-	Contents    string   `json:"contents"`
-	Description string   `json:"description"`
-	Tags        []string `json:"tags"`
 }
 
 func GetSpell(c *gin.Context) {
@@ -31,65 +24,38 @@ func GetSpell(c *gin.Context) {
 func CreateSpell(language string, contents string, description string, tags string) (int, error) {
 	db := Utils.GetDatabaseConnection()
 	// var newSpell SpellDBModel
-	newSpell := SpellDBModel{
+	newSpell := Spell{
 		Language: language, Contents: contents, Description: description,
 		Tags: tags,
 	}
-	db.Table("Spells").Create(&newSpell)
+	db.Table(tableName).Create(&newSpell)
 	return newSpell.ID, nil
 }
 
 func GetAllSpells() ([]Spell, error) {
 	db := Utils.GetDatabaseConnection()
-	var dbResults []SpellDBModel
-	db.Table("Spells").Find(&dbResults)
 	var results []Spell
-	for _, spell := range dbResults {
-		spellToAppend := Spell{
-			ID:       spell.ID,
-			Language: spell.Language, Contents: spell.Contents, Description: spell.Description,
-			Tags: strings.Split(spell.Tags, " "),
-		}
-		results = append(results, spellToAppend)
-	}
+	db.Table(tableName).Find(&results)
 	return results, nil
 }
 
 func FindSpellsByTag(tag string) ([]Spell, error) {
 	db := Utils.GetDatabaseConnection()
-	var dbResults []SpellDBModel
 	var results []Spell
 	fmt.Printf("args in FindSpellsByTags %s\n", tag)
 
 	// db.Table("Spells").Where("tags LIKE ?", "%?%", tags).Find(&dbResults)
-	db.Table("Spells").Where("tags LIKE ?", "%"+tag+"%").Find(&dbResults)
-	for _, spell := range dbResults {
-		spellToAppend := Spell{
-			ID:       spell.ID,
-			Language: spell.Language, Contents: spell.Contents, Description: spell.Description,
-			Tags: strings.Split(spell.Tags, " "),
-		}
-		results = append(results, spellToAppend)
-	}
+	db.Table(tableName).Where("tags LIKE ?", "%"+tag+"%").Find(&results)
+
 	return results, nil
 }
 
 func GetSpellByID(spell_id int) (Spell, error) {
 	db := Utils.GetDatabaseConnection()
-	var dbresult []SpellDBModel
 	fmt.Printf("ID in GetSpellbyID: %d\n", spell_id)
 	var result Spell
 	// db.Table("Spells").Where("tags LIKE ?", "%?%", tags).Find(&dbResults)
-	db.Table("Spells").Find(&dbresult, spell_id)
-	if len(dbresult) == 0 {
-		return result, nil
-	}
-	result = Spell{
-		ID:       dbresult[0].ID,
-		Language: dbresult[0].Language, Contents: dbresult[0].Contents, Description: dbresult[0].Description,
-		Tags: strings.Split(dbresult[0].Tags, " "),
-	}
-
+	db.Table(tableName).Find(&result, spell_id)
 	return result, nil
 }
 
